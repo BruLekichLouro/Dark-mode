65aeabee55395bb1d716f8682b235103d7bbfd2a
BruLekichLouro <bububio@yahoo.com.br>
2022-08-17 10:40:30 -0300

    Adicionando itens no html e estilo

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Atividade dark-mode/light-mode
+function chamaTemaEscuro(){
+
+    document.getElementsByTagName('h1 footer button body').toggle("dark-mode");
+    var mudaTexto = "Dark mode ON";
+    document.getElementsByTagName('button').innerHTML = mudaTexto;
+
+}
